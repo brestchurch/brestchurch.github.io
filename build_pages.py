a965336d4611970f484e0de956c40f29c9c42f7b
brestchurch <simplkostya@gmail.com>
2026-09-14 17:19:10 +0300

    Update build_pages.py

--- build_pages.py
+++ build_pages.py
@@ -307,6 +307,7 @@ for route, fname, title, desc in PAGES:
                         ('__LOGO_BLACK__','assets/logo-black.png'),
                         ('__FAVICON__','assets/favicon.png'),
                         ('__P_SIMONCHIK__','assets/person-simonchik.jpg'),
+                        ('__P_MARINA__','assets/person-marina.jpg'),
                         ('__P_KONYUCHKO__','assets/person-konyuchko.jpg'),
                         ('__P_VEREMCHUK_D__','assets/person-veremchuk-d.jpg'),
                         ('__P_DEMIDOVICH__','assets/person-demidovich.jpg'),
@@ -327,7 +328,7 @@ for route, fname, title, desc in PAGES:
 
 # ---------- картинки ----------
 os.makedirs(os.path.join(OUT, 'assets'), exist_ok=True)
-for f in ['logo-white.png','logo-black.png','favicon.png','apple-touch-icon.png','erip-qr.png','person-simonchik.jpg',
+for f in ['logo-white.png','logo-black.png','favicon.png','apple-touch-icon.png','erip-qr.png','person-simonchik.jpg','person-marina.jpg',
           'person-konyuchko.jpg','person-veremchuk-d.jpg','person-demidovich.jpg',
           'person-roy.jpg','person-veremchuk-dan.jpg',
           'person-ivan-asya.jpg','person-nika.jpg','person-kristina.jpg','person-igor.jpg',
